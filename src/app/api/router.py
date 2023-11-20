f769706d746737406357defb0128e43026fb45c1
Abylay Kuvatov <abukuvatov@icloud.com>
2023-11-20 16:41:26 +0600

    The functions of the router have been changed to work with the database

--- src/app/api/router.py
+++ src/app/api/router.py
@@ -1,42 +1,39 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import Any
+from db.models import Issue, Issue_Pydantic, IssueIn_Pydantic
 
 
 router = APIRouter(prefix='/api')
 
 
-issues = [
-    {'id': 1, 'name': 'do something 1', 'deadline': '01.01.2024'},
-    {'id': 2, 'name': 'do something 2', 'deadline': '02.02.2024'},
-    {'id': 3, 'name': 'do something 3', 'deadline': '03.03.2024'},
-]
+class Status(BaseModel):
+    message: str
 
 
-@router.get('/issues')
-def get_issues() -> list[dict]:
-    return issues
+@router.get('/issues', response_model=list[Issue_Pydantic])
+async def get_issues() -> list[dict]:
+    return await Issue_Pydantic.from_queryset(Issue.all())
 
 
-@router.get('/issues/{id_}')
-def get_issue(issue_id: int) -> list[dict]:
-    return [issue for issue in issues if issue.get('id') == issue_id]
+@router.post('/issues', response_model=Issue_Pydantic)
+async def create_issue(issue: IssueIn_Pydantic):
+    issue_obj = await Issue.create(**issue.model_dump(exclude_unset=True))
+    return await Issue_Pydantic.from_tortoise_orm(issue_obj)
 
 
-class Issue(BaseModel):
-    id: int
-    name: str
-    deadline: str
+@router.get('/issue/{id_}', response_model=Issue_Pydantic)
+async def get_issue(issue_id: int) -> list[dict]:
+    return await Issue_Pydantic.from_queryset_single(Issue.get(id=issue_id))
 
 
-@router.post('/issues')
-def add_issue(issues_list: list[Issue]) -> list[Any]:
-    issues.extend(issues_list)
-    return issues
+@router.put('/issue/{id_}', response_model=Issue_Pydantic)
+async def update_issue(issue_id: int, issue: IssueIn_Pydantic):
+    await Issue.filter(id=issue_id).update(**issue.model_dump(exclude_unset=True))
+    return await Issue_Pydantic.from_queryset_single(Issue.get(id=issue_id))
 
 
-@router.post('/issues/{id_}')
-def edit_issue(issue_id: int, new_name: str) -> dict[str, Any]:
-    issue = [issue for issue in issues if issue.get('id') == issue_id][0]
-    issue['name'] = new_name
-    return issue
+@router.delete('/issue/{id_}', response_model=Status)
+async def delete_issue(issue_id: int):
+    await Issue.filter(id=issue_id).delete()
+    return Status(message=f"Deleted issue {issue_id}")
--- src/app/db/models.py
+++ src/app/db/models.py
@@ -1,8 +1,15 @@
 from tortoise import models, fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class Issue(models.Model):
     id = fields.IntField(pk=True)
-    name = fields.TextField()
+    name = fields.CharField(max_length=30)
     deadline = fields.DateField()
+
+    def __str__(self) -> str:
+        return self.name
     
+
+Issue_Pydantic = pydantic_model_creator(Issue, name='Issue')
+IssueIn_Pydantic = pydantic_model_creator(Issue, name="IssueIn", exclude_readonly=True)
