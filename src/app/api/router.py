6af67d9e9cb138be4be74843c3736fbb08e85363
Abylay Kuvatov <abukuvatov@icloud.com>
2023-11-21 19:30:39 +0600

    Added .idea to the .gitignore

--- src/app/api/router.py
+++ src/app/api/router.py
@@ -4,7 +4,7 @@ from typing import Any
 from db.models import Issue, Issue_Pydantic, IssueIn_Pydantic
 
 
-router = APIRouter(prefix='/api')
+router = APIRouter(prefix='/api', tags=['issue'])
 
 
 class Status(BaseModel):
@@ -17,23 +17,23 @@ async def get_issues() -> list[dict]:
 
 
 @router.post('/issues', response_model=Issue_Pydantic)
-async def create_issue(issue: IssueIn_Pydantic) -> dict[Any]:
+async def create_issue(issue: IssueIn_Pydantic) -> Issue:
     issue_obj = await Issue.create(**issue.model_dump(exclude_unset=True))
     return await Issue_Pydantic.from_tortoise_orm(issue_obj)
 
 
 @router.get('/issue/{id_}', response_model=Issue_Pydantic)
-async def get_issue(issue_id: int) -> dict[Any]:
+async def get_issue(issue_id: int) -> Issue:
     return await Issue_Pydantic.from_queryset_single(Issue.get(id=issue_id))
 
 
 @router.put('/issue/{id_}', response_model=Issue_Pydantic)
-async def update_issue(issue_id: int, issue: IssueIn_Pydantic) -> dict[Any]:
+async def update_issue(issue_id: int, issue: IssueIn_Pydantic) -> Issue:
     await Issue.filter(id=issue_id).update(**issue.model_dump(exclude_unset=True))
     return await Issue_Pydantic.from_queryset_single(Issue.get(id=issue_id))
 
 
 @router.delete('/issue/{id_}', response_model=Status)
-async def delete_issue(issue_id: int) -> dict[Any]:
+async def delete_issue(issue_id: int) -> str:
     await Issue.filter(id=issue_id).delete()
     return Status(message=f"Deleted issue {issue_id}")
